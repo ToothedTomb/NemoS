5c4e8b4b0233c92bcfc5113620492960e769ef14
Jonathan Steadman <52569279+ToothedTomb@users.noreply.github.com>
2025-01-12 17:50:35 +0300

    Add install code for more popular distros.
    If it fails please install xclip manually but for most people it will work.
    
    There are thousands of distros, so its very hard to add all the commands to install application. 
    
    It now has installations for:
    1) Void Linux.
    2) Alphine Linux.
    3) Gentoo linux.
    This is needed to get xclip working. :)

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 
 # Ensure the script is being run with root privileges
 if [[ $EUID -ne 0 ]]; then
-  echo "This script must be run as root." >&2
+  echo "Error 1: This script must be run as root." >&2
   exit 1
 fi
 
@@ -17,6 +17,11 @@ fi
 sudo apt install xclip
 sudo packman -S install xclip
 sudo dnf install xclip
+sudo zypper install xclip
+sudo xbps-install -S xclip
+sudo apk add xclip
+sudo emerge xclip
+
 echo "If the install for xclip is not working and dont have it installed. Please look online installing xclip for your distrobution!"
 # Copy the binary to /usr/local/bin
 cp nemos /usr/local/bin/
