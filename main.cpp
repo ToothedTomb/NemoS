70299346700f15617eeb60f5cdc62f75ffdd77c5
Jonathan Steadman <52569279+ToothedTomb@users.noreply.github.com>
2025-04-12 17:13:31 +0300

    Fixed a bug when resizing.

--- main.cpp
+++ main.cpp
@@ -40,7 +40,7 @@ SOFTWARE.
 #include <ctime> // Will help display the time at the bottom.
 #include <cctype>
 #include <algorithm>
-
+#include <signal.h>
 
 
 
@@ -156,7 +156,7 @@ private:
     void drawHelp() {
         clear();
         attron(COLOR_PAIR(3));
-        mvprintw(1, 1, "Help Menu for NemoS 3.3:");
+        mvprintw(1, 1, "Help Menu for NemoS 3.4:");
         mvprintw(3, 1, "Arrow Keys: Navigate");
         mvprintw(4, 1, "Enter: Insert new line");
         mvprintw(5, 1, "Backspace: Delete character");
@@ -394,7 +394,7 @@ private:
 
 
 
-            mvprintw(LINES - 1, 0, "NemoS 3.3 | File: %s | Word Count: %d | Line: %d | Column: %d | Ctrl+H: Help | Ctrl+X: Exit ", filename.c_str(),wordCount, cursorY + 1, cursorX +1);
+            mvprintw(LINES - 1, 0, "NemoS 3.4 | File: %s | Word Count: %d | Line: %d | Column: %d | Ctrl+H: Help | Ctrl+X: Exit ", filename.c_str(),wordCount, cursorY + 1, cursorX +1);
             attroff(COLOR_PAIR(2));
             cursorX = std::min(cursorX, (int)content[cursorY].size());
             cursorY = std::min(cursorY, (int)content.size() -1);
@@ -406,6 +406,14 @@ private:
             int maxX = std::max(0, (int)content[cursorY].size() - visiblewidth); // Correct maxX            getmaxyx(stdscr, height,width);
             int ch = getch(); // Get user input
             switch (ch) {
+                case KEY_RESIZE: //This should fix the resize -z bug from before...
+                resizeterm(0, 0);
+                if (cursorY >= viewY + LINES - 1)
+                    viewY = std::max(0, cursorY - (LINES - 2));
+                if (cursorX >= viewX + COLS - 1)
+                    viewX = std::max(0, cursorX - (COLS - 2));
+                clear();  // Clear and redraw the screen
+                break;
                 case KEY_UP:
                     if (cursorY > 0) {
                         cursorY--;
